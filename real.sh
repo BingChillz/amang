ce20f993bf535f1cbb0d6d011d3cac32e7bd5ce9
BingChillz <146423397+BingChillz@users.noreply.github.com>
2024-08-28 03:14:06 +0530

    Create real.sh

---
+++ real.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+wget https://github.com/sr2echa/thottathukiduven-v2/archive/refs/heads/main.zip -O thottathukiduven-v2.zip
+unzip thottathukiduven-v2.zip
+
+wget https://github.com/0xRad1ant/Enable-Copy-Paste-neocolab/archive/refs/heads/main.zip/ -O main.zip
+unzip main.zip
+
+google-chrome --load-extension="$(pwd)/thottathukiduven-v2-main","$(pwd)/Enable-Copy-Paste-neocolab-main/Package" --user-data-dir="$(pwd)/gyatt"
