ad2cdc6d3c53422233fb18831bee518b064124da
BingChillz <146423397+BingChillz@users.noreply.github.com>
2024-08-28 03:22:01 +0530

    Update real.sh

--- real.sh
+++ real.sh
@@ -6,4 +6,4 @@ unzip thottathukiduven-v2.zip
 wget https://github.com/0xRad1ant/Enable-Copy-Paste-neocolab/archive/refs/heads/main.zip/ -O main.zip
 unzip main.zip
 
-google-chrome --load-extension="$(pwd)/thottathukiduven-v2-main","$(pwd)/Enable-Copy-Paste-neocolab-main/Package" --user-data-dir="$(pwd)/gyatt"
+google-chrome --load-extension="$(pwd)/thottathukiduven-v2-main","$(pwd)/Enable-Copy-Paste-neocolab-main/Package"
