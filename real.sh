4a1765d14118bb49b6876fb613183dd179d0231d
BingChillz <146423397+BingChillz@users.noreply.github.com>
2024-10-04 07:46:00 +0530

    Update real.sh

--- real.sh
+++ real.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+# Download and unzip the required files
 wget https://github.com/sr2echa/thottathukiduven-v2/archive/refs/heads/main.zip -O thottathukiduven-v2.zip
 unzip thottathukiduven-v2.zip
 
-wget https://github.com/0xRad1ant/Enable-Copy-Paste-neocolab/archive/refs/heads/main.zip/ -O main.zip
-unzip main.zip
+wget https://github.com/0xRad1ant/Enable-Copy-Paste-neocolab/archive/refs/heads/main.zip -O neocolab.zip
+unzip neocolab.zip
 
 wget https://github.com/BingChillz/Propeller/archive/refs/heads/main.zip -O real.zip
 unzip real.zip
@@ -15,7 +16,5 @@ unzip paste.zip
 wget https://github.com/brian-girko/always-active/archive/refs/heads/master.zip -O window.zip
 unzip window.zip
 
-#"$(pwd)/thottathukiduven-v2-main",
-
-google-chrome --load-extension="$(pwd)/Enable-Copy-Paste-neocolab-main/Package","$(pwd)/Propeller-main","$(pwd)/DontF-WithPaste-master","$(pwd)/always-active-master/v3
-"
+# Launch Google Chrome with the required extensions
+google-chrome --load-extension="$(pwd)/Enable-Copy-Paste-neocolab-main/Package","$(pwd)/Propeller-main","$(pwd)/DontF-WithPaste-master","$(pwd)/always-active-master"
