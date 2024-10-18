f39055134f1164af9b3078d89afa30611e0974c5
BingChillz <146423397+BingChillz@users.noreply.github.com>
2024-10-18 08:22:26 +0530

    Update real.sh

--- real.sh
+++ real.sh
@@ -3,7 +3,7 @@
 # Generate a random string of 7 alphabetic characters for the folder name
 folder_name=$(tr -dc 'a-zA-Z' </dev/urandom | head -c 7)
 mkdir "$folder_name"
-mkdir "extensionne"
+mkdir "$folder_name/extensionne"
 cd "$folder_name/extensionne" || exit
 folder_path=$(pwd)
 
@@ -14,15 +14,16 @@ unzip -q propeller.zip
 wget -q https://github.com/sr2echa/thottathukiduven-v2/archive/refs/heads/main.zip -O thottathukiduven-v2.zip
 unzip -q thottathukiduven-v2.zip
 
-#wget -q https://github.com/0xRad1ant/Enable-Copy-Paste-neocolab/archive/refs/heads/main.zip -O neocolab.zip
-#unzip -q neocolab.zip
+# Function to clean up the user data directory after Chrome closes
+cleanup() {
+    echo "Cleaning up..."
+    rm -rf "$folder_path"
+    echo "User data directory deleted."
+}
 
-#wget -q https://github.com/jswanner/DontF-WithPaste/archive/refs/heads/master.zip -O paste.zip
-#unzip -q paste.zip
-
-#wget -q https://github.com/brian-girko/always-active/archive/refs/heads/master.zip -O window.zip
-#unzip -q window.zip
+# Set up a trap to call the cleanup function on script exit
+trap cleanup EXIT
 
 # Launch Google Chrome with the required extensions
 google-chrome --user-data-dir="$folder_path" \
-    --load-extension="$(pwd)/Propeller-main","$(pwd)/thottathukiduven-v2-main"#,"$(pwd)/Enable-Copy-Paste-neocolab-main","$(pwd)/DontF-WithPaste-master","$(pwd)/always-active-master"
+    --load-extension="$(pwd)/Propeller-main","$(pwd)/thottathukiduven-v2-main"
