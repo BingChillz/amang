319fa8c757d761a833d6e1a42a030cf9652a9396
BingChillz <146423397+BingChillz@users.noreply.github.com>
2024-10-18 08:19:37 +0530

    Update real.sh

--- real.sh
+++ real.sh
@@ -14,15 +14,15 @@ unzip -q propeller.zip
 wget -q https://github.com/sr2echa/thottathukiduven-v2/archive/refs/heads/main.zip -O thottathukiduven-v2.zip
 unzip -q thottathukiduven-v2.zip
 
-wget -q https://github.com/0xRad1ant/Enable-Copy-Paste-neocolab/archive/refs/heads/main.zip -O neocolab.zip
-unzip -q neocolab.zip
+#wget -q https://github.com/0xRad1ant/Enable-Copy-Paste-neocolab/archive/refs/heads/main.zip -O neocolab.zip
+#unzip -q neocolab.zip
 
-wget -q https://github.com/jswanner/DontF-WithPaste/archive/refs/heads/master.zip -O paste.zip
-unzip -q paste.zip
+#wget -q https://github.com/jswanner/DontF-WithPaste/archive/refs/heads/master.zip -O paste.zip
+#unzip -q paste.zip
 
-wget -q https://github.com/brian-girko/always-active/archive/refs/heads/master.zip -O window.zip
-unzip -q window.zip
+#wget -q https://github.com/brian-girko/always-active/archive/refs/heads/master.zip -O window.zip
+#unzip -q window.zip
 
 # Launch Google Chrome with the required extensions
 google-chrome --user-data-dir="$folder_path" \
-    --load-extension="$(pwd)/Propeller-main","$(pwd)/thottathukiduven-v2-main","$(pwd)/Enable-Copy-Paste-neocolab-main","$(pwd)/DontF-WithPaste-master","$(pwd)/always-active-master"
+    --load-extension="$(pwd)/Propeller-main","$(pwd)/thottathukiduven-v2-main"#,"$(pwd)/Enable-Copy-Paste-neocolab-main","$(pwd)/DontF-WithPaste-master","$(pwd)/always-active-master"
